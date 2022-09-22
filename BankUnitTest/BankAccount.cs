5fcf8f31e66711b5b33b03d679a24365fccec78b
Zealand-Jean <55512813+Zealand-Jean@users.noreply.github.com>
2022-09-22 12:15:40 +0200

    Add project files.

---
+++ BankUnitTest/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankUnitTest
+{
+    public class BankAccount
+    {
+        #region Instance fields
+        private double _balance;
+        #endregion
+
+        #region Constructor
+        public BankAccount()
+        {
+            _balance = 0.0;
+        }
+        #endregion
+
+        #region Properties
+        public double Balance
+        {
+            get { return _balance; }
+        }
+        #endregion
+
+        #region Methods
+        public void Deposit(double amount)
+        {
+            throw new NotImplementedException();           
+        }
+
+        public void Withdraw(double amount)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ BankUnitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankUnitTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
